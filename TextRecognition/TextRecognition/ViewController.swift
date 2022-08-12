7dd6bfc6e75ef0837b2239ea2101ab5c96ba3ffc
Rumeysa Tan <tanrumeysa93@gmail.com>
2022-08-12 12:16:40 +0300

    initial commit

---
+++ TextRecognition/TextRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextRecognition
+//
+//  Created by Rumeysa TAN on 12.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TextRecognition/TextRecognitionUITests/TextRecognitionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TextRecognitionUITestsLaunchTests.swift
+//  TextRecognitionUITests
+//
+//  Created by Rumeysa TAN on 12.08.2022.
+//
+
+import XCTest
+
+class TextRecognitionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
